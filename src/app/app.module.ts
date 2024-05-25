8caba2c2a2c9acac26e46741ff79119a0715ac48
kalyan <kammari.pavankalyan@mphasis.com>
2024-05-25 19:29:42 +0530

    sample commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule, isStandalone } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ResetComponent } from './pages/reset/reset.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ROUTES, Route, RouterModule, Routes, provideRouter } from '@angular/router';
+import { Path } from 'mongoose';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { provideHttpClient} from '@angular/common/http';
+
+
+
+const appRoutes : Routes=[
+  {path: 'login' , component: LoginComponent },
+  {path: 'register' , component:RegisterComponent},
+  {path: 'forget-password', component:ForgetPasswordComponent},
+  {path: 'home', component:HomeComponent}
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetComponent,
+    ForgetPasswordComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule
+   
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideHttpClient() ],
+  bootstrap: [ AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+ 
+}
